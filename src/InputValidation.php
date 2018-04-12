871b6cf71377963e9aa10ff1fe3109a0e0c1350a
dean.haines <dean.haines@freetimers.com>
2018-04-12 17:17:10 +0100

    password tweak

--- src/InputValidation.php
+++ src/InputValidation.php
@@ -32,11 +32,11 @@ class InputValidation
         'uk_mobile' => "/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/",
         'number' => "~^[0-9]+$~",
         'text' => "~^[a-z0-9\s]+$~i",
-        'password1' => "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", #Minimum eight characters, at least one letter and one number
-        'password2' => "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$",#Minimum eight characters, at least one letter, one number and one special character
-        'password3' => "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", #Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
-        'password4' => "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}", #Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
-        'password5' => "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}" #Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+        'password1' => "~^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$~", #Minimum eight characters, at least one letter and one number
+        'password2' => "~^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$~",#Minimum eight characters, at least one letter, one number and one special character
+        'password3' => "~^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$~", #Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
+        'password4' => "~^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}~", #Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+        'password5' => "~^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}~" #Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character
     ];
 
     /**
